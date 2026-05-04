690d1984fdef83e26038274b10568a5f197cb0d4
eliott-colin <eliott.colin.pro@gmail.com>
2026-05-04 11:43:36 +0200

    fix/error cors

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  baseline: '/musicdle-frontend/',
+  base: '/musicdle-frontend/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
