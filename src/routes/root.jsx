79f000c57097ec1ede49ecd5d51fa13a7f1e4a09
TatianaAll <tatiana.allery@orange.fr>
2026-05-26 12:15:56 +0200

    feat: implement registration form with validation and styling; replace old ConnexionForm

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,8 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
 import Game from "../pages/Game.jsx";
-import ConnexionForm from "../pages/ConnexionForm.jsx";
-import SpotifyCallback from "../pages/SpotifyCallback.jsx";
+import ConnexionForm from "../pages/RegisterForm.jsx";
 import Error from "../pages/Error.jsx";
 
 const router = createBrowserRouter([
@@ -14,8 +13,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: < Home /> },
       { path: "/game/:id", element: <Game /> },
-      { path: "/spotify/callback", element: <SpotifyCallback /> },
-      { path: "/register", element: <ConnexionForm /> },
+      { path: "/register", element: <RegisterForm /> },
     ]
   }
 ],
