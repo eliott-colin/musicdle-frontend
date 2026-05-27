401f2f15ef6e93e329e4a455e718f9dc8b8b55e3
TatianaAll <tatiana.allery@orange.fr>
2026-05-27 13:18:20 +0200

    feat: enhance DailyGame component layout and styles; add play button and illustration

--- src/components/DailyGame.jsx
+++ src/components/DailyGame.jsx
@@ -9,17 +9,31 @@ function DailyGame() {
   });
   const currentDay = formattedDate.split(" ")[0];
   const currentMonth = formattedDate.split(" ")[1];
+
   return (
-    <Link to={`/game/${currentDay}`} style={{ 'textDecoration' : 'none' , color : 'black' }}>
-    <div className="bg-daily-game">
-      <div className="date-container">
-        <span className="current-day">{currentDay} </span>
-        {currentMonth}
-      </div>
-      <span className="challenge-name">Challenge du jour</span>
-      <span>❯</span>
-    </div>
-    </Link>
+    <>
+      <Link
+        to={`/game/${currentDay}`}
+        style={{ textDecoration: "none", color: "black" }}
+      >
+        <div className="daily-card">
+          {/* illustration */}
+          <div className="illustration">ILLUSTRATION</div>
+
+          <div className="bg-daily-game">
+            <div className="date-container">
+              <span className="current-day">{currentDay} </span>
+              {currentMonth}
+            </div>
+
+            <div className="right-content">
+              <span className="challenge-name">Challenge du jour</span>
+              <button className="play-btn">JOUER</button>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </>
   );
 }
 
